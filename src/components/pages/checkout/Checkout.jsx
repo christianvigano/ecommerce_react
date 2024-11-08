965edfd06db7a18a643b455b7189f35ff086fe85
IntegracionesFlexxus <integracionesflexxus@gmail.com>
2024-11-08 10:17:19 -0300

    agregado await, estilados y popup

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,121 +1,182 @@
-import { useContext, useState } from "react"
-import { db } from "../../../../firebaseConfig"
-import { CartContext } from "../../../context/CartContext"
+import { useContext, useState } from "react";
+import { db } from "../../../../firebaseConfig";
+import { CartContext } from "../../../context/CartContext";
 import { collection, addDoc, updateDoc, doc } from "firebase/firestore";
-import { TextField, Button, Box } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Box,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 import "./checkout.css";
-
+import { Link } from "react-router-dom";
 
 const Checkout = () => {
-
-  const { cart, totalCompra } = useContext(CartContext);
-
-  const [info,setInfo] = useState({name:"",lastname:"",email:""})
-
-  const [orderId, setOrdeId] = useState(null);
+  //usecontext para tomar el carrito y el total de compra
+  const { cart, totalCompra, resetCarrito } = useContext(CartContext);
   
-  
-  const enviarFormulario = (evento) => { 
+  //estado informacion para capturar en el formulario
+  const [info, setInfo] = useState({ name: "", lastname: "", email: "" });
 
+  //estado de la orden para armar el envio del pedido a firebase
+  const [orderId, setOrderId] = useState(null);
+  
+  //estado del loading
+  const [loading, setLoading] = useState(false);
+  
+  //estado del dialog que devuelve el id.
+  const [openDialog, setOpenDialog] = useState(false); // Controla el diálogo de confirmación
+
+  const enviarFormulario = async (evento) => {
     evento.preventDefault();
-    console.log(info);
+    setLoading(true);
 
+    //funcion que se ejecuta 1 vez al enviar el formulario para tener el total del carrito.
     const total = totalCompra();
-
-  const orders = {
-
+    
+    //objeto orden para setear el estado.
+    const orders = {
       buyer: info,
       items: cart,
-      total
-  }
+      total,
+    };
 
-    let refColection = collection(db, "order");
-    addDoc(refColection, orders).then((res) => setOrdeId(res.id));
+    try {
+      //referencio la coleccion de order
+      const refColection = collection(db, "order");
 
+      //agrego un registro de compra con un await para esperar la respuesta.
+      const res = await addDoc(refColection, orders);
+      //seteo ele stado order para mostrar el resultado.
+      setOrderId(res.id);
 
-let refColectionActualiza = collection(db, "products");
-   
-    
-    
-    orders.items.forEach((item) => {
-      
-      let docs = doc(refColectionActualiza, item.id);
-      
-    updateDoc(docs,{stock: item.stock-item.quantity});
+      // Abre el diálogo de confirmación
+      setOpenDialog(true);
 
-    })
-  }
+      //referencio la coleccion de productos para actualizar el stock
+      const refColectionActualiza = collection(db, "products");
+
+      //crear todas las promese de items que estan en el docs y las ejecuta en paralelo 
+      //y espera que termine todas para continuar con el sioguiente loque
+      await Promise.all(
+        orders.items.map((item) => {
+          const docs = doc(refColectionActualiza, item.id);
+          return updateDoc(docs, { stock: item.stock - item.quantity });
+        })
+      );
+    } catch (error) {
+      console.error("Error al procesar la compra:", error);
+    } finally {
+      setTimeout(2500);
+      setLoading(false);
+      resetCarrito();
+    }
+  };
 
   const capturarInfo = (evento) => {
-//desructuro la propiesda del evento.
     const { name, value } = evento.target;
-  
-    //spread para traer el objeto del estado y reemplazar variables.
-    //los 3 puntos iniciales significan, esparcime el objeto del use state y remmplazalo
-    // 
-    setInfo({...info,[name]:value  });
+    setInfo({ ...info, [name]: value });
+  };
 
-  }
+  const handleCloseDialog = () => {
+    setOpenDialog(false); // Cierra el diálogo cuando se hace clic en "Aceptar"
+  };
 
-  if (orderId) {
-    
+  return (
+    <>
+      <Box
+        component="form"
+        onSubmit={enviarFormulario}
+        className="form-container"
+      >
+        {loading ? (
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="30vh"
+            width="30%"
+          >
+            <CircularProgress />
+          </Box>
+        ) : (
+          <>
+            <div className="items-carga">
+              <TextField
+                fullWidth
+                label="Nombre"
+                name="name"
+                variant="outlined"
+                onChange={capturarInfo}
+                required
+                className="form-item"
+              />
+            </div>
 
- 
-    return <h2>Gracias por su compra: {orderId}</h2>
-  }
+            <div className="items-carga">
+              <TextField
+                fullWidth
+                label="Apellido"
+                name="lastname"
+                variant="outlined"
+                onChange={capturarInfo}
+                required
+                className="form-item"
+              />
+            </div>
 
- return (
-   <Box component="form" onSubmit={enviarFormulario} className="form-container">
-     <div className="items-carga">
-       <TextField
-         fullWidth
-         label="Nombre"
-         name="name"
-         variant="outlined"
-         onChange={capturarInfo}
-         required
-         className="form-item"
-       />
-     </div>
+            <div className="items-carga">
+              <TextField
+                fullWidth
+                label="Email"
+                type="email"
+                name="email"
+                variant="outlined"
+                onChange={capturarInfo}
+                required
+                className="form-item"
+              />
+            </div>
 
-     <div className="items-carga">
-       <TextField
-         fullWidth
-         label="Apellido"
-         name="lastname"
-         variant="outlined"
-         onChange={capturarInfo}
-         required
-         className="form-item"
-       />
-     </div>
+            <div className="boton">
+              <Button
+                className="form-button"
+                variant="contained"
+                color="primary"
+                type="submit"
+                disabled={loading}
+              >
+                Comprar
+              </Button>
+            </div>
+          </>
+        )}
+      </Box>
 
-     <div className="items-carga">
-       <TextField
-         fullWidth
-         label="Email"
-         type="email"
-         name="email"
-         variant="outlined"
-         onChange={capturarInfo}
-         required
-         className="form-item"
-       />
-     </div>
-
-     <div className="boton">
-   
-       <Button
-         className="form-button"
-         variant="contained"
-         color="primary"
-         type="submit"
-       >
-         Comprar
-       </Button>
-     </div>
-   </Box>
- );
-}
+      {/* Diálogo de confirmación */}
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogTitle>¡Gracias por su compra!</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Su orden ha sido registrada con éxito. El ID de su compra es:{" "}
+            <strong>{orderId}</strong>.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Link to="/">
+            <Button onClick={handleCloseDialog} color="primary" autoFocus>
+              Aceptar
+            </Button>
+          </Link>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
 
 export default Checkout;
